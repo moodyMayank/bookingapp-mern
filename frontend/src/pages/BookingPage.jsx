2cd4042fa868ddc618f4597cf088abcf1265f3d5
Mayank <mayankbudhiraja88@gmail.com>
2023-03-30 23:46:04 +0530

    Refatored the code

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import AddressLink from "../components/AddressLink";
+import PlaceGallery from "../components/PlaceGallery";
+import BookingDates from "../components/BookingDates";
 
 const BookingPage = () => {
   const { id } = useParams();
@@ -8,13 +11,33 @@ const BookingPage = () => {
 
   useEffect(() => {
     if (id) {
-      axios.get('/bookings').then((response) => {
-        const foundBooking = response.data.find(({_id}) => _id === id);
-        if(foundBooking)
+      axios.get("/bookings").then((response) => {
+        const foundBooking = response.data.find(({ _id }) => _id === id);
+        if (foundBooking) {
+          setBooking(foundBooking);
+        }
       });
     }
-  }, []);
-  return <div>Single Booking page</div>;
+  }, [id]);
+
+  if (!booking) return "";
+  return (
+    <div className="my-2">
+      <h1 className="text-3xl">{booking.place?.title}</h1>
+      <AddressLink className="my-2 block">{booking.place?.address}</AddressLink>
+      <div className="bg-slate-300 p-6 my-6 rounded-2xl flex items-center justify-between">
+        <div>
+          <h2 className="text-2xl mb-4">Your booking information: </h2>
+          <BookingDates booking={booking} />
+        </div>
+        <div className="bg-primary p-6 text-white rounded-2xl">
+          <div>Total Price</div>
+          <div className="text-3xl">₹{booking.price}</div>
+        </div>
+      </div>
+      <PlaceGallery place={booking.place} />
+    </div>
+  );
 };
 
 export default BookingPage;
