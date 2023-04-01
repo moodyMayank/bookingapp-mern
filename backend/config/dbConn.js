0a2db4bf8dabc0e0edfcaed98e1b23c9e8b6d0bf
Mayank <mayankbudhiraja88@gmail.com>
2023-04-01 14:24:50 +0530

    fixed URL issue

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DATABASE_URI, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
